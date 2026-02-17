7f39bfb549eb0dec4a21031df9eaf80fc52f1e66
MiltonRafael <miltonrafael166@gmail.com>
2026-02-17 14:26:02 -0300

    tratamento de excecoes

--- Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -26,7 +27,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.get();
+		Category entity = obj.orElseThrow(() ->new EntityNotFoundException("nao encontrado"));
 		return new CategoryDTO(entity);
 	}
 }
