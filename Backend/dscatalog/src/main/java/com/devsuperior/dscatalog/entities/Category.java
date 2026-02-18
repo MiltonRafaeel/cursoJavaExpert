745d2fc39e6c542987d1b1e3912989bcb5dde543
MiltonRafael <miltonrafael166@gmail.com>
2026-02-18 07:56:40 -0300

    adicionando data e hora ao salvar ou atualizar uma classe Category

--- Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +22,12 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 
 	public Category() {
 		
@@ -44,6 +54,28 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	//esse metodo tem a funcao de salvar o tempo atual quando eu salvar uma categoria
+	
+	@PrePersist
+	public void prePersist() {
+		 createdAt = Instant.now();
+	}
+	
+	//esse metodo tem a funcao de salvar o tempo atual quando eu atualizar uma categoria
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
