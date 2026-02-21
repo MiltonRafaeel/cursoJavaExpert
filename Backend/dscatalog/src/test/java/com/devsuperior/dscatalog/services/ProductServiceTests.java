4f9168fb6b54dda81a9c2be9ce21f41d131fb45d
MiltonRafael <miltonrafael166@gmail.com>
2026-02-21 12:25:39 -0300

    simulando comportamentos diversos com mockito - findAll, findById e save

--- Backend/dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ Backend/dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,18 +1,26 @@
 package com.devsuperior.dscatalog.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -26,17 +34,33 @@ public class ProductServiceTests {
 	private long existId;
 	private long nonExistId;
 	private long dependentId;
+	private PageImpl<Product> page;
+	private Product product;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		existId = 1L;
 		nonExistId = 1000L;
 		dependentId = 5L;
+		product = Factory.createProduct();
+		page = new PageImpl<>(List.of(product));
 		
+		//quando -> argumento -> acao(quando acontecer algo, passando o argumento, faca isso(retorne page))
+		Mockito.when(repository.findAll( (Pageable) ArgumentMatchers.any())).thenReturn(page);
+		
+		//quando -> argumento -> acao(quando acontecer algo, passando o argumento, faca isso(retorne product))
+		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+		
+		//aqui simulei duas situacoes, quando encontro o objeto por id e quando nao encontro.
+		Mockito.when(repository.findById(existId)).thenReturn(Optional.of(product));
+		Mockito.when(repository.findById(nonExistId)).thenReturn(Optional.empty());
+
+		//quando -> acao(quando acontecer algo, faca isso)
 		Mockito.when(repository.existsById(existId)).thenReturn(true);
 		Mockito.when(repository.existsById(nonExistId)).thenReturn(false);
 		Mockito.when(repository.existsById(dependentId)).thenReturn(true);
 
+		//acao -> quando(digo a acao quando ocorrer algo, isso quando o retorno e void, como no caso do delete)
 		Mockito.doNothing().when(repository).deleteById(existId);
 		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
 	}
