67c382b107e53dd54efde6665afb43166dfabdcf
MiltonRafael <miltonrafael166@gmail.com>
2026-02-24 17:46:21 -0300

    teste de integracao desde a camada Web - findAll

---
+++ Backend/dscatalog/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dscatalog.resources;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductResourceIT {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	private Long existId;
+	private Long nonExistId;
+	private Long countTotalProduct;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existId = 1L;
+		nonExistId = 1000L;
+		countTotalProduct = 25L;
+	}
+
+	@Test
+	public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+		
+		ResultActions result = 
+				mockMvc.perform(get("/products?page=0&size=12&sort=name,asc")
+						.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.totalElements").value(countTotalProduct));
+		result.andExpect(jsonPath("$.content").exists());
+		result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+		result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+		result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+	}
+}
