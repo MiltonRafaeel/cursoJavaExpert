874e00f3fe4eb1f025afa4108487691005400690
MiltonRafael <miltonrafael166@gmail.com>
2026-02-25 07:53:40 -0300

    teste EmployeeControllerIT - insert

--- Desafio-Empregados-tdd/src/main/java/com/devsuperior/demo/services/EmployeeService.java
+++ Desafio-Empregados-tdd/src/main/java/com/devsuperior/demo/services/EmployeeService.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.demo.dto.EmployeeDTO;
+import com.devsuperior.demo.entities.Department;
 import com.devsuperior.demo.entities.Employee;
 import com.devsuperior.demo.repositories.EmployeeRepository;
+
 
 @Service
 public class EmployeeService {
@@ -15,9 +18,19 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeRepository repository;
 	
+	@Transactional(readOnly = true)
 	public Page<EmployeeDTO> findAll(Pageable pageable) {
 		Page<Employee> result = repository.findAll(pageable);
 		return result.map(x -> new EmployeeDTO(x));
+	}
+
+	public EmployeeDTO insert(EmployeeDTO dto) {
+		Employee entity = new Employee();
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.setDepartment(new Department(dto.getDepartmentId(), null));
+		entity = repository.save(entity);
+		return new EmployeeDTO(entity);
 	}
 
 }
