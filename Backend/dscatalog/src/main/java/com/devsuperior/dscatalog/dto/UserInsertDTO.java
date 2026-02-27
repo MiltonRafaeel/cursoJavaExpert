2145d26fc4e27d3d824a802c9fc1b122cd076a41
MiltonRafael <miltonrafael166@gmail.com>
2026-02-27 15:57:40 -0300

    crud user - alterado o metodo insert para gerar o hash da senha (no momento endpoints nao autorizados)

---
+++ Backend/dscatalog/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.dto;
+
+public class UserInsertDTO extends UserDTO {
+	private static final long serialVersionUID = 1L;
+	
+	private String password;
+	
+	public UserInsertDTO() {
+		
+	}
+
+	public UserInsertDTO(Long id, String firstName, String lastName, String email, String password) {
+		super(id, firstName, lastName, email);
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
