d50276f4d1a1fa49df6e5201e13ce18fc21d4fe1
MiltonRafael <miltonrafael166@gmail.com>
2026-03-14 10:06:58 -0300

    teste - insert com dados validos do produto

--- exemplomockspy/src/test/java/com/devsuperior/exemplomockspy/services/ProductServiceTests.java
+++ exemplomockspy/src/test/java/com/devsuperior/exemplomockspy/services/ProductServiceTests.java
@@ -2,12 +2,16 @@ package com.devsuperior.exemplomockspy.services;
 
 import static org.mockito.ArgumentMatchers.any;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.devsuperior.exemplomockspy.dto.ProductDTO;
 import com.devsuperior.exemplomockspy.entities.Product;
@@ -30,17 +34,32 @@ public class ProductServiceTests {
 	
 	@BeforeEach
 	void setUp() throws Exception {
+		MockitoAnnotations.openMocks(this);
 		
 		product = new Product(1L, "Playstation", 2000.0);
 		productDto = new ProductDTO(product);
 		
 		existId = 1L;
 		nonExistId = 2L;
-		
+				
 		Mockito.when(repository.save(any())).thenReturn(product);
 		
 		Mockito.when(repository.getReferenceById(existId)).thenReturn(product);
 		
 		Mockito.when(repository.getReferenceById(nonExistId)).thenThrow(EntityNotFoundException.class);
+	}
+	
+	@Test
+	public void insertShouldReturnProductDTOWhenValidDate() {
+		
+		ProductService productSpy = Mockito.spy(service);
+		ReflectionTestUtils.setField(productSpy, "repository", repository);
+		
+		Mockito.doNothing().when(productSpy).validateData(productDto);
+		
+		ProductDTO result = productSpy.insert(productDto);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getName(), "Playstation");
 	}
 }
