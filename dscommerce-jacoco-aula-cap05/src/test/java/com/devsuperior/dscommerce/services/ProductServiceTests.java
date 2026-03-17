c9c964a900a69735ff4ea853e2648a20a8b9c07e
MiltonRafael <miltonrafael166@gmail.com>
2026-03-17 17:38:17 -0300

    teste unitario ProductService - busca por id  nao existente

--- dscommerce-jacoco-aula-cap05/src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
+++ dscommerce-jacoco-aula-cap05/src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscommerce.tests.ProductFactory;
 
 @ExtendWith(SpringExtension.class)
@@ -40,6 +41,8 @@ public class ProductServiceTests {
 		product = ProductFactory.createProduct(productName);
 		
 		Mockito.when(repository.findById(existId)).thenReturn(Optional.of(product));
+		
+		Mockito.when(repository.findById(nonExistId)).thenReturn(Optional.empty());
 	}
 	
 	@Test
@@ -50,6 +53,14 @@ public class ProductServiceTests {
 		Assertions.assertNotNull(result);
 		Assertions.assertEquals(result.getId(), existId);
 		Assertions.assertEquals(result.getName(), product.getName());
+	}
+	
+	@Test
+	public void findByIdShoudReturnResourceNotFoundExceptionWhenIdDoesNotExist() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.findById(nonExistId);
+		});
 	}
 
 }
