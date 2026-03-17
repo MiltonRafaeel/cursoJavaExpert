562d2151ef5fd7db5789a114880d4c948fe0b053
MiltonRafael <miltonrafael166@gmail.com>
2026-03-17 17:26:47 -0300

    teste unitario ProductService - busca por id existente

---
+++ dscommerce-jacoco-aula-cap05/src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
@@ -0,0 +1,55 @@
+package com.devsuperior.dscommerce.services;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.tests.ProductFactory;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+	
+	@InjectMocks
+	private ProductService service;
+	
+	@Mock
+	private ProductRepository repository;
+	
+	private Long existId, nonExistId;
+	
+	private String productName;
+	
+	private Product product;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existId = 1L;
+		nonExistId = 2L;
+		productName = "Playstation 5";
+		
+		product = ProductFactory.createProduct(productName);
+		
+		Mockito.when(repository.findById(existId)).thenReturn(Optional.of(product));
+	}
+	
+	@Test
+	public void findByIdShouldReturnProductDTOWhenIdExists() {
+		
+		ProductDTO result = service.findById(existId);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getId(), existId);
+		Assertions.assertEquals(result.getName(), product.getName());
+	}
+
+}
