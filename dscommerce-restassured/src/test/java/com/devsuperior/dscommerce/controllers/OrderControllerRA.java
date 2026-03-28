f618d0ed17941172bb51594b3f176e4f6abad4c1
MiltonRafael <miltonrafael166@gmail.com>
2026-03-28 14:43:13 -0300

    teste OrderControllerRA - findById deve retornar pedido existente quando logado com admin

---
+++ dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/OrderControllerRA.java
@@ -0,0 +1,57 @@
+package com.devsuperior.dscommerce.controllers;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.devsuperior.dscommerce.tests.TokenUtil;
+
+import io.restassured.http.ContentType;
+
+public class OrderControllerRA {
+	
+	private String clientUsername, clientPassword, adminUsername, adminPassword;
+	private String clientToken, adminToken, invalidToken;
+	private Long exstingOrderId, nonExistingOrderId;
+	
+	@BeforeEach
+	public void setUp() {
+		baseURI = "http://localhost:8080";
+		
+		exstingOrderId = 1L;
+		nonExistingOrderId = 100L;
+		
+		clientUsername = "maria@gmail.com";
+		clientPassword = "123456";
+		adminUsername = "alex@gmail.com";
+		adminPassword = "123456";
+		
+		clientToken = TokenUtil.obtainAccesToken(clientUsername, clientPassword);
+		adminToken = TokenUtil.obtainAccesToken(adminUsername, adminPassword);
+		invalidToken = adminToken + "xpto";
+	}
+	
+	@Test
+	public void findByIdShouldReturnOrderWhenIdExistsAndAdminLogged() {
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + adminToken)
+			.accept(ContentType.JSON)
+		.when()
+			.get("/orders/{id}", exstingOrderId)
+		.then()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("moment", equalTo("2022-07-25T13:00:00Z"))
+			.body("status", equalTo("PAID"))
+			.body("client.name", equalTo("Maria Brown"))
+			.body("payment.moment", equalTo("2022-07-25T15:00:00Z"))
+			.body("items.name[0]", equalTo("The Lord of the Rings"))
+			.body("items.name[1]", equalTo("Macbook Pro"))
+			.body("total", is(1431.0F));
+	}
+
+}
