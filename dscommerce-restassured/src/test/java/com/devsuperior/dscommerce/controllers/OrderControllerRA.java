9fd42f9520c00d4bc5f8d11de199edab685d961e
MiltonRafael <miltonrafael166@gmail.com>
2026-03-29 08:57:21 -0300

    teste OrderControllerRA - inserts

--- dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/OrderControllerRA.java
+++ dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/OrderControllerRA.java
@@ -1,9 +1,16 @@
 package com.devsuperior.dscommerce.controllers;
 
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,9 +20,11 @@ import io.restassured.http.ContentType;
 
 public class OrderControllerRA {
 	
-	private String clientUsername, clientPassword, adminUsername, adminPassword;
-	private String clientToken, adminToken, invalidToken;
+	private String clientUsername, clientPassword, adminUsername, adminPassword, adminOnlyUsername, adminOnlyPassword;
+	private String clientToken, adminToken, invalidToken, adminOnlyToken;
 	private Long exstingOrderId, nonExistingOrderId;
+	
+	Map<String, Object> postOrderInstance;
 	
 	@BeforeEach
 	public void setUp() {
@@ -28,10 +37,30 @@ public class OrderControllerRA {
 		clientPassword = "123456";
 		adminUsername = "alex@gmail.com";
 		adminPassword = "123456";
+		adminOnlyUsername = "milton@gmail.com";
+		adminOnlyPassword = "123456";
 		
 		clientToken = TokenUtil.obtainAccesToken(clientUsername, clientPassword);
 		adminToken = TokenUtil.obtainAccesToken(adminUsername, adminPassword);
+		adminOnlyToken = TokenUtil.obtainAccesToken(adminOnlyUsername, adminOnlyPassword);
 		invalidToken = adminToken + "xpto";
+		
+		postOrderInstance = new HashMap<>();
+		List<Map<String, Object>> items = new ArrayList<>();
+		
+		Map<String, Object> list0 = new HashMap<>();
+		list0.put("productId", 1);
+		list0.put("quantity", 2);
+		
+		items.add(list0);
+		
+		Map<String, Object> list1 = new HashMap<>();
+		list1.put("productId", 5);
+		list1.put("quantity", 1);
+		
+		items.add(list1);
+		
+		postOrderInstance.put("items", items);
 	}
 	
 	@Test
@@ -120,6 +149,73 @@ public class OrderControllerRA {
 			.accept(ContentType.JSON)
 		.when()
 			.get("/orders/{id}", exstingOrderId)
+		.then()
+			.statusCode(401);
+	}
+	
+	@Test
+	public void insertShouldReturnOrderWhenClientLogged() {
+		JSONObject newOrder = new JSONObject(postOrderInstance);
+		
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + clientToken)
+			.body(newOrder)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post("/orders")
+		.then()
+			.statusCode(201)
+			.body("id", is(4));
+	}
+	
+	@Test
+	public void insertShouldReturnUnprocessableEntityWhenInvalidItemsAndClientLogged() {
+		postOrderInstance.put("items", null);
+		JSONObject newOrder = new JSONObject(postOrderInstance);
+		
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + clientToken)
+			.body(newOrder)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post("/orders")
+		.then()
+			.statusCode(422)
+			.body("errors.message[0]", equalTo("Deve ter pelo menos um item"));
+	}
+	
+	@Test
+	public void insertShouldReturnForbbidenWhenAdminLogged() {
+		JSONObject newOrder = new JSONObject(postOrderInstance);
+		
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + adminOnlyToken)
+			.body(newOrder)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post("/orders")
+		.then()
+			.statusCode(403);
+	}
+	
+	@Test
+	public void insertShouldReturnUnauthorizedWhenInvalidToken() {
+		JSONObject newOrder = new JSONObject(postOrderInstance);
+		
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + invalidToken)
+			.body(newOrder)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post("/orders")
 		.then()
 			.statusCode(401);
 	}
