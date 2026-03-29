03a5cc28954864e64bebd30073a61092dc56dd00
MiltonRafael <miltonrafael166@gmail.com>
2026-03-29 10:11:07 -0300

    teste CategoryControllerRA - findAll

---
+++ dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/CategoryControllerRA.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dscommerce.controllers;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CategoryControllerRA {
+	
+	@BeforeEach
+	public void setUp() {
+		baseURI = "http://localhost:8080";		
+	}
+	
+	@Test
+	public void findByIdShouldReturnOrderWhenIdExistsAndAdminLogged() {
+		given()
+			.get("/categories")
+		.then()
+			.statusCode(200)
+			.body("id", hasItems(1, 2, 3))
+			.body("name", hasItems("Livros", "Eletrônicos", "Computadores"));
+	}	
+}
